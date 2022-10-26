7c55ed603de44bb3f13a8cb02b4b853fc4c36962
Tim Graupmann <tgraupmann@gmail.com>
2022-10-26 00:33:47 -0700

    Connecting model to server

--- Server_base.py
+++ Server_base.py
@@ -9,6 +9,17 @@
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import List
+import whisper
+
+print("Loading Whisper Model...")
+
+model = whisper.load_model("tiny")
+#model = whisper.load_model("base") #max size supported by Samsung ChromeBook4 with 4GB of RAM
+#model = whisper.load_model("small")
+#model = whisper.load_model("medium")
+#model = whisper.load_model("large") # too large for my PC
 
 def readServerHtml():
   htmlFile = open('Server.html', mode='r') 
@@ -18,10 +29,22 @@ def readServerHtml():
 
 app = FastAPI()
 
-@app.get('/api')
-async def index_page():
-  return {"hello":"world"}
+class TranslateItem(BaseModel):
+  data: List[float]
+
+@app.post('/translate')
+async def api_translate(item:TranslateItem):
+  print("Transcription: Start...")
+  results = model.transcribe("Test_MP3.mp3", language="en")
+  #results = model.transcribe([1,2,3], language="en")
+  waveData = item.data;
+  #print("waveData", waveData)
+  #results = model.transcribe(waveData, language="en")
+  print("Transcription: Done!")
+  return {"text":results["text"]}
 
 @app.get("/", response_class=HTMLResponse)
 async def read_items():
   return HTMLResponse(content=readServerHtml(), status_code=200)
+
+print("Whisper Server loaded!")
