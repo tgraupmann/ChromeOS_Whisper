0a341e689d8728fce6d7f98dd623dace85fd1001
Timothy Graupmann <tgraupmann@gmail.com>
2022-10-29 09:46:35 -0700

    Added form and audio capture

--- WinForms_ServerBase/Form1.Designer.cs
+++ WinForms_ServerBase/Form1.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cboAudioDevice = new System.Windows.Forms.ComboBox();
             this.pbVolume = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // cboAudioDevice
@@ -39,6 +43,7 @@
             this.cboAudioDevice.Name = "cboAudioDevice";
             this.cboAudioDevice.Size = new System.Drawing.Size(198, 23);
             this.cboAudioDevice.TabIndex = 6;
+            this.cboAudioDevice.SelectedIndexChanged += new System.EventHandler(this.cboAudioDevice_SelectedIndexChanged);
             // 
             // pbVolume
             // 
@@ -49,18 +54,51 @@
             this.pbVolume.Size = new System.Drawing.Size(323, 23);
             this.pbVolume.TabIndex = 5;
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 364);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Translations:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 100;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.cboAudioDevice);
             this.Controls.Add(this.pbVolume);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "txtTranslations";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +106,8 @@
 
         private ComboBox cboAudioDevice;
         private ProgressBar pbVolume;
+        private TextBox textBox1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- WinForms_ServerBase/Form1.cs
+++ WinForms_ServerBase/Form1.cs
@@ -1,20 +1,149 @@
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
 namespace WinForms_ServerBase
 {
     public partial class Form1 : Form
     {
+        NAudio.Wave.WaveInEvent? Wave;
+        NAudio.Wave.WasapiLoopbackCapture? CaptureDevice = null;
+
+        readonly double[] AudioValues;
+
+        readonly int SampleRate = 16000;
+        readonly int BitDepth = 16;
+        readonly int ChannelCount = 1;
+        readonly int BufferMilliseconds = 1000;
+
         public Form1()
         {
             InitializeComponent();
+            AudioValues = new double[SampleRate * BufferMilliseconds / 1000];
+        }
+
+        private void StopOutputDeviceRecording()
+        {
+            if (CaptureDevice is not null)
+            {
+                CaptureDevice.StopRecording();
+                CaptureDevice.Dispose();
+                CaptureDevice = null;
+
+                for (int i = 0; i < AudioValues.Length; i++)
+                {
+                    AudioValues[i] = 0;
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            #region Input devices
 
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                var caps = NAudio.Wave.WaveIn.GetCapabilities(i);
+                cboAudioDevice.Items.Add(caps.ProductName);
+            }
+
+            #endregion Input devices
+
+
+            #region Output devices
+
+            var enumerator = new MMDeviceEnumerator();
+            foreach (var wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+            {
+                cboAudioDevice.Items.Add(wasapi.FriendlyName);
+            }
+
+            #endregion Output devices
+        }
+
+        private void cboAudioDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StopOutputDeviceRecording();
+
+            if (Wave is not null)
+            {
+                Wave.StopRecording();
+                Wave.Dispose();
+
+                for (int i = 0; i < AudioValues.Length; i++)
+                {
+                    AudioValues[i] = 0;
+                }
+            }
+
+            if (cboAudioDevice.SelectedIndex == -1)
+                return;
+
+            if (cboAudioDevice.SelectedIndex < NAudio.Wave.WaveIn.DeviceCount)
+            {
+                Wave = new NAudio.Wave.WaveInEvent()
+                {
+                    DeviceNumber = cboAudioDevice.SelectedIndex,
+                    WaveFormat = new NAudio.Wave.WaveFormat(SampleRate, BitDepth, ChannelCount),
+                    BufferMilliseconds = BufferMilliseconds
+                };
+
+                Wave.DataAvailable += Wave_DataAvailable;
+                Wave.StartRecording();
+            }
+            else
+            {
+                #region Output devices
+
+                var enumerator = new MMDeviceEnumerator();
+                int selectedIndex = NAudio.Wave.WaveIn.DeviceCount;
+                foreach (var wasapi in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+                {
+                    if (cboAudioDevice.SelectedIndex == selectedIndex)
+                    {
+                        MMDevice loopbackCaptureDevice = wasapi;
+                        CaptureDevice = new WasapiLoopbackCapture(loopbackCaptureDevice);
+                        CaptureDevice.DataAvailable += Wave_DataAvailable;
+                        CaptureDevice.StartRecording();
+                        break;
+                    }
+                    ++selectedIndex;
+                }
+
+                #endregion Output devices
+            }
+        }
+
+        void Wave_DataAvailable(object? sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            for (int i = 0; i < e.Buffer.Length / 2; i++)
+            {
+                if (i < AudioValues.Length)
+                {
+                    AudioValues[i] = BitConverter.ToInt16(e.Buffer, i * 2);
+                }
+            }
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            StopOutputDeviceRecording();
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                int level = (int)AudioValues.Max();
+
+                // auto-scale the maximum progressbar level
+                if (level > pbVolume.Maximum)
+                    pbVolume.Maximum = level;
+                pbVolume.Value = level;
+            }
+            catch (Exception)
+            {
+                //handle hardware muting the mic
+            }
         }
     }
 }
